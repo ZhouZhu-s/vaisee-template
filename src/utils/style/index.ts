ddf12d5a2226b1c6dcd3a3fd488f1477b97d05a0
letconstvar <1985225667@qq.com>
2023-08-23 22:36:30 +0800

    chore: 优化包结构

--- src/utils/style/index.ts
+++ src/utils/style/index.ts
@@ -1,10 +1,1 @@
-/**
- * 获取指定名称的CSS变量的值。
- *
- * @param {string} name - CSS变量的名称。
- * @return {string | null} CSS变量的值，如果变量未定义，则返回null。
- */
-export const getCssVariable = (name: string): string | null => {
-  const value = getComputedStyle(document.documentElement).getPropertyValue(name);
-  return value === '' ? null : value;
-};
+export { getCssVariable } from './getCssVariable';
