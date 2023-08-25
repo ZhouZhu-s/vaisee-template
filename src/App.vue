64d5907d73fbe7b5e78241cca226da8a58ce97eb
letconstvar <1985225667@qq.com>
2023-08-25 23:00:20 +0800

    perf: 优化代码

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,11 @@ import enUS from 'ant-design-vue/es/locale/en_US';
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import dayjs from 'dayjs';
 import 'dayjs/locale/zh-cn';
-import { computed, watch } from 'vue';
+import { watch } from 'vue';
 import { theme } from 'ant-design-vue';
 import { useThemeMode } from '@/stores/themeMode';
 import { storeToRefs } from 'pinia';
 import { useCssVarStore } from '@/stores/cssStore';
-import RouterTags from '@/components/router-tags/index.vue';
 import { useKeepAlivePages } from '@/hooks/useKeepAlivePages';
 import { useLangStore } from '@/stores/lang';
 import { useI18n } from 'vue-i18n';
@@ -22,15 +21,6 @@ watch(lang, () => {
 });
 
 dayjs.locale(lang.value);
-
-const checked = computed({
-  get() {
-    return lang.value === 'en';
-  },
-  set() {
-    langStore.toggleLang();
-  }
-});
 
 const themeMode = useThemeMode();
 const { mode } = storeToRefs(themeMode);
@@ -53,11 +43,6 @@ setMaxKeepAlivePages(10);
     }"
   >
     <main class="AppMain" :mode="mode">
-      <a-switch v-model:checked="checked" />
-      <!-- <input type="color" v-model="color"> -->
-      <router-link to="/a-v">a-v</router-link>
-      <router-link to="/b-v">b-v</router-link>
-      <RouterTags></RouterTags>
       <router-view v-slot="{ Component, route }">
         <keep-alive :max="maxKeepAlivePages" :include="includePages">
           <component :is="Component" :key="route.fullPath" />
