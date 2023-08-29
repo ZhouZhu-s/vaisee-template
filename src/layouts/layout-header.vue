ce2c092e4954d6fb284e5e4453d249875f2430d9
letconstvar <1985225667@qq.com>
2023-08-29 18:08:59 +0800

    fix(views): 修复主题切换样式问题

--- src/layouts/layout-header.vue
+++ src/layouts/layout-header.vue
@@ -1,4 +1,21 @@
 <template>
-  <a-layout-header>
-  </a-layout-header>
+  <header class="layout-header">
+    <ThemeSwitch />
+  </header>
 </template>
+
+<script setup lang="ts">
+import ThemeSwitch from '@/components/theme-switch/index.vue'
+</script>
+
+<style lang="less" scoped>
+.layout-header {
+  width: 100%;
+  height: 64px;
+  background-color: var(--primary-background);
+  box-shadow: 0px 1px 6px var(--shadow-color);
+  // border-bottom: 1px solid #ffffff;
+  position: relative;
+  z-index: 1;
+}
+</style>
