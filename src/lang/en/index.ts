225ec49ffe9d357b21a637f8125c8ec0cd6f671b
letconstvar <1985225667@qq.com>
2023-09-03 00:48:25 +0800

    feat: 新增中英字典

--- src/lang/en/index.ts
+++ src/lang/en/index.ts
@@ -1,3 +1,14 @@
 export default {
+  settingPanel: 'Setting Panel',
+  language: 'Language',
+  chinese: 'Chinese',
+  english: 'English',
+  themeMode: 'ThemeMode',
+  darkMode: 'DarkMode',
+  lightMode: 'LightMode',
+  themeColor: 'ThemeColor',
+  borderRadius: 'BorderRadius',
+  detail: 'Detail',
+  syncSystem: 'Sync With System',
   login: 'login',
 }
--- src/lang/zh/index.ts
+++ src/lang/zh/index.ts
@@ -1,3 +1,14 @@
 export default {
+  settingPanel: '设置面板',
+  language: '语言',
+  chinese: '中文',
+  english: '英文',
+  themeMode: '主题模式',
+  darkMode: '深色模式',
+  lightMode: '浅色模式',
+  themeColor: '主题颜色',
+  borderRadius: '圆角大小',
+  detail: '详情',
+  syncSystem: '同步系统',
   login: '登录',
 }
