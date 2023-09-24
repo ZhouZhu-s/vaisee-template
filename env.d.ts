03d096ce1ab34b868128ef4ee56914ec7e149f74
letconstvar <1985225667@qq.com>
2023-09-24 11:24:24 +0800

    feat: 新增环境变量声明文件

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_LOCAL_SERVER_HTTPS: 'true' | 'false';
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
