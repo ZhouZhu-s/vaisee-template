8e5d045ccd6a2fbaec58f81e5565a3fef94a7486
letconstvar <1985225667@qq.com>
2023-09-24 18:10:10 +0800

    fix: localStorage中读取默认使用语言

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,22 +1,25 @@
-import { createI18n } from 'vue-i18n';
-import zh from './zh';
-import en from './en';
 import type { App } from 'vue';
+import { createI18n } from 'vue-i18n';
+import en from './en';
+import zh from './zh';
 
 export type LocaleType = 'zh-cn' | 'en';
 
 const messages: Record<LocaleType, any> = {
   'zh-cn': zh,
-  'en': en
+  en: en
 };
+
+const LangStore = localStorage.getItem('LangStore');
+const defaultLocale = LangStore ? JSON.parse(LangStore).lang || 'zh-cn' : 'zh-cn';
 
 export const i18n = createI18n({
   legacy: false, // 使用 Composition API 模式，则需要将其设置为false
   globalInjection: true, //全局生效$t
-  locale: 'zh-cn', // 默认使用的语言
+  locale: defaultLocale, // 默认使用的语言
   messages // 使用数据源
 });
 
 export const setupI18n = (app: App) => {
   app.use(i18n);
-}
+};
